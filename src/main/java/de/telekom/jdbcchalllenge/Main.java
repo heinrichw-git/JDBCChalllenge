b300c4169b9864a990fdaff1bb626ca294514eab
heinrichw-git <h.w.github@gmail.com>
2021-05-18 19:22:43 +0200

    Ergaenzung um Aenderung des Nachnamens bei einem und Loeschung eines anderen Datensatzes. Funktion noch Fehlerhaft.

--- src/main/java/de/telekom/jdbcchalllenge/Main.java
+++ src/main/java/de/telekom/jdbcchalllenge/Main.java
@@ -19,16 +19,18 @@ public class Main {
 		Class.forName(DRIVER);
 		try { 
 			Connection connection = DriverManager.getConnection(URL); 
-			System.out.println("erfolgreich!");
+			System.out.println("Connect mit der Datenbank erfolgreich!");
 			Statement statement = connection.createStatement();
 			ResultSet resultSet = statement.executeQuery("select * from personen");
 			
+			// Ausgabe der Anzahl in der Tabelle personen
 			while (resultSet.next()) {
 				System.out.println("In der Tabelle personen gibt es " + resultSet.getInt(1) + " Einträge.");
 			}
 			
+			// Ausgabe der Tabelle Personen
 			resultSet = statement.executeQuery("select * from personen");
-			
+			System.out.println("Liste der Tabelle personen:");
 			while (resultSet.next()) {
 				System.out.println("ID          : " + resultSet.getInt(1)); // ID
 				System.out.println("SALUTATION  : " + resultSet.getInt(2)); // Anrede
@@ -37,6 +39,7 @@ public class Main {
 				System.out.println("------------");
 			}
 			
+			// Eingabe einer neuen Person 
 			Scanner scanner = new Scanner(System.in);
 			System.out.println("ID: ");
 			long id = Long.parseLong(scanner.nextLine());
@@ -46,8 +49,9 @@ public class Main {
 			String name = scanner.nextLine(); 
 			System.out.println("SURNAME: ");
 			String surname = scanner.nextLine(); 
-			scanner.close();
+//			scanner.close();
 			
+			// Import einer Person in die Tabelle personen
 			PreparedStatement preparendStatement = connection.prepareStatement("INSERT INTO personen ( ID, SALUTATION, NAME, SURNAME) VALUES (?, ?, ?, ?)");
 			preparendStatement.setLong(1, id);
 			preparendStatement.setShort(2, salutation);
@@ -55,8 +59,64 @@ public class Main {
 			preparendStatement.setString(4, surname);
 			preparendStatement.execute();
 			
+			// Ausgabe der Tabelle personen
 			resultSet = statement.executeQuery("select * from personen");
+			System.out.println("Liste der Tabelle personen:");
+			while (resultSet.next()) {
+				System.out.println("ID          : " + resultSet.getInt(1)); // ID
+				System.out.println("SALUTATION  : " + resultSet.getInt(2)); // Anrede
+				System.out.println("NAME        : " + resultSet.getString(3)); // Nachname
+				System.out.println("SURNAME     : " + resultSet.getString(4)); // Vorname
+				System.out.println("------------");
+			}
 			
+			// Änderung eines Vornamens in der Tabelle personen
+			preparendStatement = connection.prepareStatement("UPDATE personen set NAME='Max' where ID=4");
+//			resultSet = statement.executeQuery("UPDATE personen set NAME='Maxi' where ID=4");
+			
+			// Ausgabe der Tabelle personen
+			resultSet = statement.executeQuery("select * from personen");
+			System.out.println("Liste der Tabelle personen:");
+			while (resultSet.next()) {
+				System.out.println("ID          : " + resultSet.getInt(1)); // ID
+				System.out.println("SALUTATION  : " + resultSet.getInt(2)); // Anrede
+				System.out.println("NAME        : " + resultSet.getString(3)); // Nachname
+				System.out.println("SURNAME     : " + resultSet.getString(4)); // Vorname
+				System.out.println("------------");
+			}
+			
+			// Eingabe der ID für eine Änderung des Nachnamen in der Tabelle personen
+			System.out.println("Änderungseingabe:");
+//			Scanner scanner = new Scanner(System.in);
+			System.out.println("ID: ");
+			id = Long.parseLong(scanner.nextLine());
+			System.out.println("New SURNAME: ");
+			surname = scanner.nextLine(); 
+			preparendStatement = connection.prepareStatement("UPDATE personen set SURNAME='" + surname + "' where ID=" + id);
+//			resultSet = statement.executeQuery("UPDATE personen set SURNAME='" + surname + "' where ID=" + id);
+			
+			scanner.close();
+			
+			// Ausgabe der Tabelle personen
+			resultSet = statement.executeQuery("select * from personen");
+			System.out.println("Liste der Tabelle personen:");
+			while (resultSet.next()) {
+				System.out.println("ID          : " + resultSet.getInt(1)); // ID
+				System.out.println("SALUTATION  : " + resultSet.getInt(2)); // Anrede
+				System.out.println("NAME        : " + resultSet.getString(3)); // Nachname
+				System.out.println("SURNAME     : " + resultSet.getString(4)); // Vorname
+				System.out.println("------------");
+			}
+			
+			// Löschen von 2 IDs aus der Tabelle Personen
+			preparendStatement = connection.prepareStatement("DELETE from personen where ID=7");
+//			preparendStatement = connection.prepareStatement("DELETE from personen where ID=7");
+//			resultSet = statement.executeQuery("DELETE from personen where ID=7");
+//			resultSet = statement.executeQuery("DELETE from personen where ID=7");
+			
+			// Ausgabe der Tabelle personen
+			resultSet = statement.executeQuery("select * from personen");
+			System.out.println("Liste der Tabelle personen:");
 			while (resultSet.next()) {
 				System.out.println("ID          : " + resultSet.getInt(1)); // ID
 				System.out.println("SALUTATION  : " + resultSet.getInt(2)); // Anrede
